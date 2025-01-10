e7e3704b3eb467c51935c3a20aa1478f1683b250
OmgRod <89850217+OmgRod@users.noreply.github.com>
2025-01-10 20:31:04 +0000

    Initial Commit

---
+++ about.md
@@ -0,0 +1,2 @@
+# External Hook API
+
---
+++ include/HookClass.hpp
@@ -0,0 +1,51 @@
+#pragma once
+
+#include <Geode/Geode.hpp>
+#include <Geode/modify/CCLayer.hpp>
+#include <Geode/modify/CCDirector.hpp>
+
+using namespace geode::prelude;
+
+#define HookClass(className)                           \
+class className : public CCLayer {                     \
+public:                                                \
+    void HookClassInit();                              \
+};                                                     \
+class $modify(CCLayer) {                               \
+    bool init() {                                      \
+        if (!CCLayer::init()) return false;            \
+        if (!Mod::get()->getSettingValue<bool>("external-mods")) { \
+            return true;                               \
+        }                                              \
+        if (auto x = typeinfo_cast<className*>(this)) {\
+            queueInMainThread([=] { x->HookClassInit(); }); \
+            return true;                               \
+        }                                              \
+        return true;                                   \
+    }                                                  \
+};                                                     \
+void className::HookClassInit()
+
+#define HookClass_Scene(unique, className)             \
+class unique##Sillyclass {                             \
+public:                                                \
+    void HookClassInit(CCNode* _This);                 \
+};                                                     \
+class $modify(CCDirector) {                            \
+    static void onModify(auto& self) {                 \
+        (void)self.setHookPriority("CCDirector::willSwitchToScene", -999); \
+    }                                                  \
+    void willSwitchToScene(CCScene* scene) {           \
+        CCDirector::willSwitchToScene(scene);          \
+        if (!Mod::get()->getSettingValue<bool>("external-mods")) { \
+            return;                                    \
+        }                                              \
+        if (CCLayer* child = scene->getChildByType<CCLayer>(0)) { \
+            if (child->getID() == className) {         \
+                unique##Sillyclass sillyInstance;      \
+                sillyInstance.HookClassInit(child);    \
+            }                                          \
+        }                                              \
+    }                                                  \
+};                                                     \
+void unique##Sillyclass::HookClassInit(CCNode* _This)
